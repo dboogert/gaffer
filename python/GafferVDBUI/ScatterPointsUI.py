90d61d03a27f60e7413e6de0d85f83375ccf27f9
Don Boogert <don.boogert@gmail.com>
2018-10-20 16:59:30 -0700

    GafferVDBUI : ScatterPoints UI metadata - FIX UP

--- python/GafferVDBUI/ScatterPointsUI.py
+++ python/GafferVDBUI/ScatterPointsUI.py
@@ -42,11 +42,41 @@ GafferUI.Metadata.registerNode(
     'description',
     """Scatter points into active voxels of VDB grid""",
     plugs={
+
+        'outputType' : [
+            'description',
+            """
+            Type of primitive to generate 
+            """,
+            "plugValueWidget:type", "GafferUI.PresetsPlugValueWidget",
+            "preset:Points Primitive", 0,
+            "preset:VDB Points", 1
+        ],
         'grid' : [
             'description',
             """
             Name of grid in VDBObject in which points will be scattered
+            """,
+        ],
+
+        'nonuniform' : [
+            'description',
             """
-        ]
+            If nonuniform the grid value is used to weight the number of points
+            """,
+        ],
+        'pointCount' : [
+            'description',
+            """
+            If 'uniform' the total number of points to generate.
+            """,
+        ],
+        'probability' : [
+            'description',
+            """
+            If 'nonuniform' the global probability which is used with the voxel value to weight the number of points.
+            """,
+        ],
+
     }
 )
