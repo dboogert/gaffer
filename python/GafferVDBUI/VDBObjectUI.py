84c8d9d48ab9789de25de016a5e22d7da723aafd
Don Boogert <don.boogert@gmail.com>
2019-12-24 19:57:51 -0800

    GafferVDBUI : VDBObject UI metadata (fix up)

--- python/GafferVDBUI/VDBObjectUI.py
+++ python/GafferVDBUI/VDBObjectUI.py
@@ -40,5 +40,31 @@ import GafferVDB
 GafferUI.Metadata.registerNode(
     GafferVDB.VDBObject,
     'description',
-    """Create a new VDB Object"""
+    """Create a new VDB Object""",
+    plugs={
+        'gridType' : [
+            'description',
+            """
+            Grid type to create (fog or level set)
+            """,
+            "plugValueWidget:type", "GafferUI.PresetsPlugValueWidget",
+            "preset:Fog Volumee", 0,
+            "preset:Level Set", 1,
+        ],
+        'gridName' : [
+            'description',
+            """
+            name of the grid to create in the VDB Object
+            """
+        ],
+        'voxelSize' : [
+            'description',
+            """
+            Size of the voxels in the empty grid. Reducing the size will increase the resolution and detail which can
+            be stored in the grid but increase memory. 
+            """
+        ]
+
+    }
 )
+
