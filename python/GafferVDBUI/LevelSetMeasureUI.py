9350c5c088a1b847438240ddecac3dceff1550be
Don Boogert <don.boogert@gmail.com>
2020-01-01 18:15:04 -0800

    GafferVDBUI : LevelSetMeasureUI - more tooltips (fix up)

--- python/GafferVDBUI/LevelSetMeasureUI.py
+++ python/GafferVDBUI/LevelSetMeasureUI.py
@@ -38,7 +38,7 @@ import GafferUI
 import GafferVDB
 
 GafferUI.Metadata.registerNode(
-    GafferVDB.Statistics,
+    GafferVDB.LevelSetMeasure,
     'description',
     """Calculate LevelSet area, volume & curvature for grids""",
     plugs={
@@ -46,6 +46,18 @@ GafferUI.Metadata.registerNode(
             'description',
             """
             Names of grids to generate measures for.
+            """
+        ],
+        'curvature' : [
+            'description',
+            """
+            Calculate the mean curvature of the level set. Note this is much slower than calculating area & volume
+            """
+        ],
+        'worldUnits' : [
+            'description',
+            """
+            Calculate the metrics using world space units instead of index space.
             """
         ]
     }
