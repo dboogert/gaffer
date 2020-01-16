5e35c9ff9719fc87c30dcd4a2e4837a07732d0b3
Don Boogert <don.boogert@gmail.com>
2020-01-15 23:10:29 -0800

    tests

--- python/GafferVDBTest/SphereLevelSetTest.py
+++ python/GafferVDBTest/SphereLevelSetTest.py
@@ -48,11 +48,18 @@ import os
 
 class SphereLevelSetTest( GafferVDBTest.VDBTestCase ) :
 
-    def testCanCreateSphereLevelSet( self ):
+    def testCanCreateSphereLevelSet( self ) :
         sphereLevelSet = GafferVDB.SphereLevelSet( "SphereLevelSet" )
         surfaceGrid = sphereLevelSet['out'].object( "vdb" ).findGrid( "surface" )
         self.assertTrue( surfaceGrid is not None )
         self.assertEqual( surfaceGrid.gridClass, "level set")
+
+    def testCanSetGridName( self ) :
+        sphereLevelSet = GafferVDB.SphereLevelSet( "SphereLevelSet" )
+        sphereLevelSet["grid"].setValue( "foo" )
+
+        surfaceGrid = sphereLevelSet['out'].object( "vdb" ).findGrid( "foo" )
+        self.assertTrue( surfaceGrid is not None )
 
     def testSignedDistanceIsNearlyZeroForPointsOnSphere( self ):
         sphereLevelSet = GafferVDB.SphereLevelSet( "SphereLevelSet" )
@@ -63,7 +70,7 @@ class SphereLevelSetTest( GafferVDBTest.VDBTestCase ) :
         self.assertEqual( accessor.probeValue( (0,10,0)) , (0.0, True)  )
         self.assertEqual( accessor.probeValue( (0,0,10)) , (0.0, True)  )
 
-    def testCanSetSphereRadius( self ):
+    def testCanSetSphereRadius( self ) :
         sphereLevelSet = GafferVDB.SphereLevelSet( "SphereLevelSet" )
         sphereLevelSet["radius"].setValue( 2.0 )
 
@@ -74,8 +81,46 @@ class SphereLevelSetTest( GafferVDBTest.VDBTestCase ) :
         self.assertEqual( accessor.probeValue( (0,20,0)) , (0.0, True)  )
         self.assertEqual( accessor.probeValue( (0,0,20)) , (0.0, True)  )
 
-    # check the leaf count updates based on voxel size, bandwidth
-    # check left count doesn't change
+    def testBounds( self ) :
+        sphereLevelSet = GafferVDB.SphereLevelSet( "SphereLevelSet" )
+
+        sphereLevelSet["radius"].setValue( 2.0 )
+        bounds = sphereLevelSet['out'].bound( "vdb")
+        self.assertEqual(bounds, imath.Box3f(imath.V3f(-2.25, -2.25, -2.25), imath.V3f(2.25, 2.25, 2.25)))
+
+        sphereLevelSet["voxelSize"].setValue( 0.2 )
+        bounds = sphereLevelSet['out'].bound( "vdb")
+        self.assertEqual(bounds, imath.Box3f(imath.V3f(-2.5, -2.5, -2.5), imath.V3f(2.5, 2.5, 2.5)))
+
+        sphereLevelSet["center"].setValue( imath.V3f(2.5, 0, 0) )
+        bounds = sphereLevelSet['out'].bound( "vdb")
+        self.assertBoundsNearlyEqual(bounds, imath.Box3f(imath.V3f(-0.1, -2.5, -2.5), imath.V3f(5.1, 2.5, 2.5)))
+
+    def testVoxelCount( self ) :
+        sphereLevelSet = GafferVDB.SphereLevelSet( "SphereLevelSet" )
+
+        originalLeafCount = sphereLevelSet['out'].object( "vdb" ).findGrid( "surface" ).leafCount()
+
+        sphereLevelSet["voxelSize"].setValue( 0.05 )
+        sphereLevelSet["halfWidth"].setValue( 6.0 )
+        newLeafCount = sphereLevelSet['out'].object( "vdb" ).findGrid( "surface" ).leafCount()
+
+        self.assertTrue( newLeafCount >  originalLeafCount )
+
+        sphereLevelSet["center"].setValue( imath.V3f(3, 0, 0) )
+        translatedLeafCount = sphereLevelSet['out'].object( "vdb" ).findGrid( "surface" ).leafCount()
+        self.assertTrue(abs(translatedLeafCount - newLeafCount) < 10)
+
+    def assertBoundsNearlyEqual(self, lhs, rhs):
+        self.assertAlmostEqual( lhs.min()[0], lhs.min()[0] )
+        self.assertAlmostEqual( lhs.min()[1], lhs.min()[1] )
+        self.assertAlmostEqual( lhs.min()[2], lhs.min()[2] )
+
+        self.assertAlmostEqual( lhs.max()[0], lhs.max()[0] )
+        self.assertAlmostEqual( lhs.max()[1], lhs.max()[1] )
+        self.assertAlmostEqual( lhs.max()[2], lhs.max()[2] )
+
+
 
 if __name__ == "__main__":
     unittest.main()
